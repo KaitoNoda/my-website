7285bd8bb1364c31f3b3eaab6efb19a093e8171e
kaitonoda <kaito.noda19@gmail.com>
2023-05-30 16:35:25 +0900

    背景カラーの変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Header from 'components/Header/Header';
-import Footer from 'components/Footer/Footer';
-import Introduction from 'components/Introduction/Introduction';
-import Portfolios from 'components/Portfolios/Portfolios';
-import Timeline from 'components/Timeline/Timeline';
-import Skills from 'components/Skills/Skills';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import Introduction from 'components/Introduction';
+import Portfolios from 'components/Portfolios';
+import Timeline from 'components/Timeline';
+import Skills from 'components/Skills';
 
 const Home:React.FC = () =>{
     return (
-        <div style = {{backgroundColor:'lightblue'}}>
+        <div style = {{backgroundColor:'#e3dddd'}}>
             <Header/>
             <Introduction/>
             <Portfolios/>
