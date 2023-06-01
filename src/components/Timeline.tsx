fe6946c3c90d8babe506242afa95e9fa5da9311d
kaitonoda <kaito.noda19@gmail.com>
2023-06-01 18:44:28 +0900

    Timeline コンポーネントのデザインと実装

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,19 +1,132 @@
-import React from 'react';
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-const Timeline: React.FC = () => {
-    return (
-        <div id ="timeline" 
-            style={{
-            backgroundColor: 'blue',
-            width: '300px',
-            height: '300px',
-            margin: '50px auto',
-            padding: '20px',
-            color: 'white',
-        }}>
-            <p>Timeline</p>
-        </div>
-    )
+export default function OppositeContentTimeline() {
+  return (
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          1999
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper
+            elevation={4}
+            sx={{ backgroundColor: "#e3dddd", padding: "10px", width: "400px" }}
+          >
+            <Typography>
+              I was born in the culturally rich city of Nagoya, Japan
+            </Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2017
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Box display="flex" justifyContent="right">
+            <Paper
+              elevation={4}
+              sx={{
+                backgroundColor: "#e3dddd",
+                padding: "10px",
+                width: "400px",
+              }}
+            >
+              <Typography>
+                Eager to gain practical experience, I secured a jobat the
+                Marriott Hotel in Nagoya where I worked for the next three
+                years.
+              </Typography>
+            </Paper>
+          </Box>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2018
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper
+            elevation={4}
+            sx={{ backgroundColor: "#e3dddd", padding: "10px", width: "400px" }}
+          >
+            <Typography>
+              A milestone year for me as I successfully graduated from high
+              school in my hometown, Nagoya, and continuing my job at the
+              Marriott.
+            </Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2020
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Box display="flex" justifyContent="right">
+            <Paper
+              elevation={4}
+              sx={{
+                backgroundColor: "#e3dddd",
+                padding: "10px",
+                width: "400px",
+              }}
+            >
+              <Typography>
+                Eager to see the world and to pursue higher education, I
+                embarked on a new adventure, relocating to Canada.
+              </Typography>
+            </Paper>
+          </Box>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2022
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper
+            elevation={4}
+            sx={{ backgroundColor: "#e3dddd", padding: "10px", width: "400px" }}
+          >
+            <Typography>
+              As a significant step towards my academic and career goals, I
+              enrolled in a Bachelor of Science degree program at the
+              prestigious University of British Columbia in Canada.
+            </Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
 }
-
-export default Timeline;
