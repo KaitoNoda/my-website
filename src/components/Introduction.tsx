149492a90d8b5e0321546b4dc0d9b94f447771a1
kaitonoda <kaito.noda19@gmail.com>
2023-06-01 10:45:13 +0900

    レイアウトの最終的な調節

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -7,28 +7,26 @@ const Introduction: React.FC = () => {
       sx={{
       display: 'flex',
       backgroundColor: '#e3dddd',
-      width: '800px',
+      width: '1000px',
       height: '350px',
       margin: '150px auto',
       color: 'black', 
     }}>
-      <Box sx={{ flex: '1' }}>
+      <Box sx={{ flex: '0.7' }} > 
         <h1 style={{ fontSize:'40px'}}>Here is who I am</h1>
         <p style={{ fontSize:'20px'}}> First of all, 
             thank you for visiting my website. I am glad 
             you took this opportunity to know me more. I am originally from Japan and am currently a 
-           <strong> computer 
+            <strong> computer 
             science student </strong> at the University of British Columbia (UBC). 
             My goal is to become an expert <strong>software engineer</strong> specializing in
             web development. </p>
       </Box>
-      <Box sx={{ flex: '1' }}>
-        <img src="/face.png" style={{ width: '100%',height: 'auto' }} />
+      <Box sx={{ flex: '0.3' }}> 
+        <img src="/face.png" style={{ height: '100%' }} />
       </Box>
     </Box>
   );
 }
 
 export default Introduction;
-
-
