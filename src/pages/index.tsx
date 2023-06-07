c1b294eaa72ec0b4804c48824aefb544e5830d14
kaitonoda <kaito.noda19@gmail.com>
2023-06-07 13:01:58 +0900

    export default からexportに変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import Introduction from 'components/Introduction';
-import Portfolios from 'components/Portfolios';
-import Timeline from 'components/Timeline';
-import Skills from 'components/Skills';
-import { CssBaseline } from '@mui/material';
+import React from "react";
+import { Header } from "components/Header";
+import Footer from "components/Footer";
+import Introduction from "components/Introduction";
+import Portfolios from "components/Portfolios";
+import Timeline from "components/Timeline";
+import Skills from "components/Skills";
+import { CssBaseline } from "@mui/material";
 
 const Home: React.FC = () => {
-    return (
-        <>
-            <CssBaseline />
-            <div style={{
-                backgroundColor: '#e3dddd',
-                margin: 0,
-                padding: 0,
-                boxSizing: 'border-box',
-            }}>
-                <Header />
-                <Introduction />
-                <Timeline />
-                <Skills />
-                <Portfolios />
-                <Footer />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <CssBaseline />
+      <div
+        style={{
+          backgroundColor: "#e3dddd",
+          margin: 0,
+          padding: 0,
+          boxSizing: "border-box",
+        }}
+      >
+        <Header />
+        <Introduction />
+        <Portfolios />
+        <Timeline />
+        <Skills />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default Home;
