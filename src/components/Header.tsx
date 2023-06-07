35b11a3647a9204b6bb3ae1b545b0ec62279ec1a
kaitonoda <kaito.noda19@gmail.com>
2023-06-07 12:59:56 +0900

    変数を別途に切り分けする

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Box from '@mui/system/Box';
-import { styled } from '@mui/system';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/system/Box";
+import { styled } from "@mui/system";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { COLOR_CODES } from "constants/color-codes";
 
 const HeaderButton = styled(Button)({
-  color: "#111111",
+  color: COLOR_CODES.BLACK,
   textDecoration: "none",
   marginRight: "20px",
   position: "relative",
@@ -19,7 +20,7 @@ const HeaderButton = styled(Button)({
     left: "50%",
     width: "0",
     height: "2px",
-    backgroundColor: "#111111",
+    backgroundColor: COLOR_CODES.BLACK,
     transition: "all 0.3s",
     transform: "translateX(-50%)",
   },
@@ -28,43 +29,55 @@ const HeaderButton = styled(Button)({
   },
 });
 
-const IconButton = styled('a')({
-  color: "#111111",
+const IconButton = styled("a")({
+  color: COLOR_CODES.BLACK,
   marginLeft: "20px",
   cursor: "pointer",
   transition: "color 0.3s",
   "&:hover": {
-    color: "gray",
+    color: COLOR_CODES.GRAY,
   },
 });
 
 const Header: React.FC = () => {
   return (
-    <Box sx={{
-      backgroundColor: "#e3dddd",
-      padding: "30px",  
-      boxSizing: "border-box",
-    }}>
-      <Box sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems:"center",
-        width: "70%",
-        borderBottom: "1px solid #111111",
-        margin: "0 auto",
-      }}>
+    <Box
+      sx={{
+        backgroundColor: COLOR_CODES.BG,
+        padding: "30px",
+        boxSizing: "border-box",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "70%",
+          borderBottom: "1px solid #111111",
+          margin: "0 auto",
+        }}
+      >
         <Box sx={{ display: "flex" }}>
-          <HeaderButton href = "#aboutMe">About Me</HeaderButton>
-          <HeaderButton href = "#timeline">Timeline</HeaderButton>
-          <HeaderButton href = "#portfolios">Portfolios</HeaderButton>
-          <HeaderButton href = "#skills">Skills</HeaderButton>
-          <HeaderButton href = "#contact">Contact</HeaderButton>
+          <HeaderButton href="#aboutMe">About Me</HeaderButton>
+          <HeaderButton href="#timeline">Timeline</HeaderButton>
+          <HeaderButton href="#portfolios">Portfolios</HeaderButton>
+          <HeaderButton href="#skills">Skills</HeaderButton>
+          <HeaderButton href="#contact">Contact</HeaderButton>
         </Box>
         <Box sx={{ display: "flex" }}>
-          <IconButton href="https://github.com/kaitonoda" target="_blank" rel="noopener noreferrer">
+          <IconButton
+            href="https://github.com/kaitonoda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGithub size={40} />
           </IconButton>
-          <IconButton href="https://linkedin.com/in/kaitonoda" target="_blank" rel="noopener noreferrer">
+          <IconButton
+            href="https://linkedin.com/in/kaitonoda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin size={40} />
           </IconButton>
         </Box>
@@ -74,4 +87,3 @@ const Header: React.FC = () => {
 };
 
 export default Header;
-
