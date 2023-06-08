772d395ce0a1b1845d3cd58b8bfe4d8d74ee0117
kaitonoda <kaito.noda19@gmail.com>
2023-06-08 11:44:26 +0900

    h1やpタグをTypographyに変更する

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Box from "@mui/system/Box";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import Image from "next/image";
 
 const Introduction: React.FC = () => {
@@ -10,6 +11,7 @@ const Introduction: React.FC = () => {
       sx={{
         display: "flex",
         backgroundColor: "#e3dddd",
+        pl: "10px",
         width: "1000px",
         height: "350px",
         margin: "150px auto",
@@ -17,14 +19,23 @@ const Introduction: React.FC = () => {
       }}
     >
       <Box flex={"0.7"} mt={"10px"} ml={"20px"}>
-        <h1 style={{ fontSize: "40px" }}>Here is who I am</h1>
-        <p style={{ fontSize: "20px" }}>
+        <Typography
+          variant={"h1"}
+          component={"h1"}
+          fontSize={"40px"}
+          fontWeight={"bold"}
+          mt={"30px"}
+          mb={"30px"}
+        >
+          Here is who I am
+        </Typography>
+        <Typography variant={"body1"} component={"p"} fontSize={"20px"}>
           First of all, thank you for visiting my website. I am glad you took
           this opportunity to know me more. I am originally from Japan and am
           currently a <strong>computer science student</strong> at the
           University of British Columbia (UBC). My goal is to become an expert
-          <strong>software engineer</strong> specializing in web development.
-        </p>
+          <strong> software engineer</strong> specializing in web development.
+        </Typography>
       </Box>
       <Box flex={"0.3"}>
         <Image src="/face.png" alt="My face pic" width={420} height={350} />
