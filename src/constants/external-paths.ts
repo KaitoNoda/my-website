5250edaf9569379937519874ac327450c6baed25
kaitonoda <kaito.noda19@gmail.com>
2023-06-09 11:28:41 +0900

    別ファイルにパスやカラーの値をまとめる

--- src/constants/external-paths.ts
+++ src/constants/external-paths.ts
@@ -1,3 +1,9 @@
 export const EXTERNAL_PATHS = {
   FACE: "/face.png",
+  IMG1: "/image1.jpg",
+  IMG2: "/image2.jpg",
+  IMG3: "/image3.jpg",
+  IMG4: "/image4.jpg",
+  IMG5: "/image5.jpg",
+  IMG6: "/image6.jpg",
 };
